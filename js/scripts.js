7d026e4a761dc5d4bb91d7441c852ed23e739e30
Elly Maimon <eliranmaimon@gmail.com>
2018-06-01 12:44:03 -0700

    revert changes on q4 image js and styling to fix bugs

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,11 @@ function tally(a1, a2, a3) {
 
 
 $(function(){
-
   var answer1 = parseInt($("input:radio[name=q1]:checked").val());
-
   var answer2 = parseInt($("input:radio[name=q2]:checked").val());
+  var answer3 = parseInt($("input:radio[name=q3]:checked").val());
+  var answer4 = parseInt($("input:radio[name=q4]:checked").val());
 
-  var answer1 = parseInt($("input:radio[name=q3]:checked").val());
-
-  var answer1 = parseInt($("input:radio[name=q4]:checked").val());
-
-  $('#question4 input:radio').addClass('input_hidden');
-  $('#question4 label').click(function(){
-      $(this).addClass('selected').siblings().removeClass('selected');
-  });
 
 
 });
