e591c021a542c7aac63ab96ee3370aefa693b9e5
Elly Maimon <eliranmaimon@gmail.com>
2018-06-01 13:54:32 -0700

    add name field and variable for username is js

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,14 @@ $(function(){
   $("form#survey").submit(function(event) {
     event.preventDefault();
 
+    // var userName = ("input#nameInput").val();
     var answer1 = parseInt($("input:radio[name=q1]:checked").val());
     var answer2 = parseInt($("input:radio[name=q2]:checked").val());
     var answer3 = parseInt($("input:radio[name=q3]:checked").val());
     var answer4 = parseInt($("input:radio[name=q4]:checked").val());
     var answer5 = parseInt($("input:radio[name=q5]:checked").val());
 
-    alert(answer1);
+    // alert(userName);
     alert(answer2);
     alert(answer3);
     alert(answer4);
