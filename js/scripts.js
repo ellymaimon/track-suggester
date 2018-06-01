cb1c5f079d0ae4be2bf92e7f05d7169e7ea6826d
Elly Maimon <eliranmaimon@gmail.com>
2018-06-01 13:27:46 -0700

    turn question 5 checkboxes into radio buttons to fix bug, add javascript variable returns for questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
-function tally(a1, a2, a3) {
-  return a1 + a2 + a3;
+function tally(a1, a2, a3, a4, a5) {
+  return a1 + a2 + a3 + a4 + a5;
 }
 
 
 
 
 $(function(){
-  var answer1 = parseInt($("input:radio[name=q1]:checked").val());
-  var answer2 = parseInt($("input:radio[name=q2]:checked").val());
-  var answer3 = parseInt($("input:radio[name=q3]:checked").val());
-  var answer4 = parseInt($("input:radio[name=q4]:checked").val());
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
 
+    var answer1 = parseInt($("input:radio[name=q1]:checked").val());
+    var answer2 = parseInt($("input:radio[name=q2]:checked").val());
+    var answer3 = parseInt($("input:radio[name=q3]:checked").val());
+    var answer4 = parseInt($("input:radio[name=q4]:checked").val());
+    var answer5 = parseInt($("input:radio[name=q5]:checked").val());
 
-
+    alert(answer1);
+    alert(answer2);
+    alert(answer3);
+    alert(answer4);
+    alert(answer5);
+  });
 });
